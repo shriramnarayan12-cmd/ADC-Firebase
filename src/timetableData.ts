d46867a6b538b54339486fe9f97f8ea9f2df193c
shriramnarayan12-cmd <shriramnarayan12@gmail.com>
2026-06-21 21:01:54 +0530

    Update timetableData.ts

--- src/timetableData.ts
+++ src/timetableData.ts
@@ -13,7 +13,7 @@ export const timetableData: Record<string, { StudioA: any[], StudioB: any[] }> =
   },
   "Tuesday": {
     StudioA: [
-      { time: "18:00:00", batch: "2nd year Kids Kathak 2", form: "Kathak", faculty: "Bhavana/ Ananya" },
+      { time: "6:00 PM", batch: "2nd year Kids Kathak 2", form: "Kathak", faculty: "Bhavana/ Ananya" },
       { time: "7:00 PM", batch: "3rd Year Kids - (Sanmathi Batch)", form: "Kathak", faculty: "Spurthi" },
       { time: "8:00 PM", batch: "3rd Year Adults - (Darshan Batch)", form: "Kathak", faculty: "Spurthi" }
     ],
@@ -25,7 +25,7 @@ export const timetableData: Record<string, { StudioA: any[], StudioB: any[] }> =
   "Wednesday": {
     StudioA: [
       { time: "5:00PM", batch: "1st year KTK kids 2025", form: "Kathak", faculty: "Harshitha" },
-      { time: "18:00:00", batch: "5th year kids - (Aarabhi Batch)", form: "Kathak", faculty: "Rohini P" },
+      { time: "6:00 PM", batch: "5th year kids - (Aarabhi Batch)", form: "Kathak", faculty: "Rohini P" },
       { time: "7:00 PM", batch: "Mix 1 + Senior - (Saanchi Batch) + 6th Year Adults - (Sanika Batch)", form: "Kathak", faculty: "NR" }
     ],
     StudioB: [
@@ -39,7 +39,7 @@ export const timetableData: Record<string, { StudioA: any[], StudioB: any[] }> =
       { time: "5:00 PM", batch: "3rd year BTYM kids", form: "Bntym", faculty: "Medhavini" },
       { time: "6:00 PM", batch: "2nd year Btym Kids 1", form: "Bntym", faculty: "Meera" },
       { time: "7:00 PM", batch: "1st year Btym Kids 2025", form: "Bntym", faculty: "Manasa" },
-      { time: "17:00:00", batch: "2026 BTYM batch", form: "Bntym", faculty: "Indu" }
+      { time: "5:00 PM", batch: "2026 BTYM batch", form: "Bntym", faculty: "Indu" }
     ],
     StudioB: [
       { time: "5:00 PM", batch: "4rd Year Kids - (Sakshya Batch)", form: "Kathak", faculty: "Pooja" },
@@ -52,38 +52,38 @@ export const timetableData: Record<string, { StudioA: any[], StudioB: any[] }> =
       { time: "7:00 PM", batch: "Mix 1 + Senior - (Saanchi Batch) + 6th Year Adults - (Sanika Batch)", form: "Kathak", faculty: "NR" }
     ],
     StudioB: [
-      { time: "18:00:00", batch: "5th year kids (Aarabhi Batch)", form: "Kathak", faculty: "Rohini P" },
+      { time: "6:00 PM", batch: "5th year kids (Aarabhi Batch)", form: "Kathak", faculty: "Rohini P" },
       { time: "7:00 PM", batch: "3rd Year Kids - (Sanmathi Batch)", form: "Kathak", faculty: "Spurthi" },
-      { time: "20:00:00", batch: "3rd Year Adults - (Darshan Batch)", form: "Kathak", faculty: "Spurthi" }
+      { time: "8:00 PM", batch: "3rd Year Adults - (Darshan Batch)", form: "Kathak", faculty: "Spurthi" }
     ]
   },
   "Saturday": {
     StudioA: [
       { time: "7:00 AM", batch: "5th Year - (Ranjan Batch)", form: "Kathak", faculty: "Apoorva" },
-      { time: "17:00:00", batch: "Jatiswara Aditi's Batch + Smruthi Adult", form: "Bntym", faculty: "Indu" },
-      { time: "18:00:00", batch: "6th Year kids - (Dyuthi Batch) + Meera", form: "Bntym", faculty: "Medhavini" },
-      { time: "19:00:00", batch: "7th Year Teenagers - (Ojas Batch) + Intermediate A", form: "Bntym", faculty: "NR" }
+      { time: "5:00 PM", batch: "Jatiswara Aditi's Batch + Smruthi Adult", form: "Bntym", faculty: "Indu" },
+      { time: "6:00 PM", batch: "6th Year kids - (Dyuthi Batch) + Meera", form: "Bntym", faculty: "Medhavini" },
+      { time: "7:00 PM", batch: "7th Year Teenagers - (Ojas Batch) + Intermediate A", form: "Bntym", faculty: "NR" }
     ],
     StudioB: [
       { time: "7:00 AM", batch: "6th+7th Year (Dimple Batch)", form: "Kathak", faculty: "Maanvi" },
-      { time: "08:00:00", batch: "2nd year Kathak adults batch 1", form: "Kathak", faculty: "Maanvi" },
-      { time: "17:00:00", batch: "1st year KTK Adults 2 2025", form: "Kathak", faculty: "Sanchita" },
-      { time: "18:00:00", batch: "Split Batch of Rasikapriya and GB", form: "Bntym", faculty: "Meera" }
+      { time: "8:00 AM", batch: "2nd year Kathak adults batch 1", form: "Kathak", faculty: "Maanvi" },
+      { time: "5:00 PM", batch: "1st year KTK Adults 2 2025", form: "Kathak", faculty: "Sanchita" },
+      { time: "6:00 PM", batch: "Split Batch of Rasikapriya and GB", form: "Bntym", faculty: "Meera" }
     ]
   },
   "Sunday": {
     StudioA: [
       { time: "7:00 AM", batch: "6th+7th Year (Dimple Batch)", form: "Kathak", faculty: "Maanvi" },
-      { time: "08:00:00", batch: "2nd year Kathak adults batch 1", form: "Kathak", faculty: "Maanvi" },
-      { time: "09:00:00", batch: "Split Batch of Rasikapriya and GB", form: "Bntym", faculty: "Meera" },
+      { time: "8:00 AM", batch: "2nd year Kathak adults batch 1", form: "Kathak", faculty: "Maanvi" },
+      { time: "9:00 AM", batch: "Split Batch of Rasikapriya and GB", form: "Bntym", faculty: "Meera" },
       { time: "10:00 AM", batch: "1st year KTK Adults 2 2025", form: "Kathak", faculty: "Sanchita" },
       { time: "11:00 AM", batch: "6th Year kids - (Dyuthi Batch) + Meera", form: "Bntym", faculty: "Medhavini" }
     ],
     StudioB: [
       { time: "7:00 AM", batch: "BTYM Seniors / OJAS Batch", form: "Bntym", faculty: "NR" },
-      { time: "08:00:00", batch: "BTYM Seniors / OJAS Batch", form: "Bntym", faculty: "NR" },
+      { time: "8:00 AM", batch: "BTYM Seniors / OJAS Batch", form: "Bntym", faculty: "NR" },
       { time: "9:00 AM", batch: "Jatiswara Aditi's Batch + Smruthi Adult", form: "Bntym", faculty: "Indu" },
-      { time: "10:00AM", batch: "5th Year - (Ranjan Batch)", form: "Kathak", faculty: "Apoorva" }
+      { time: "10:00 AM", batch: "5th Year - (Ranjan Batch)", form: "Kathak", faculty: "Apoorva" }
     ]
   }
 };
